ec9bd718caa6c180f831e1fe4f6d16c0f271ba40
Daiki Sato <sato.dik+sourcetree@gmail.com>
2014-04-10 19:36:54 +0900

    console.log()削除

--- jquery.tumblrMoArchs.js
+++ jquery.tumblrMoArchs.js
@@ -43,7 +43,6 @@
 	} // end of Plugin.prototype.init
 
 	Plugin.prototype._createArchiveList = function(data) {
-		console.log(data);
 		var self = this,
 				$responseText = $('<html>').html(data.responseText),
 				$appendTag = $('<ul>'),
